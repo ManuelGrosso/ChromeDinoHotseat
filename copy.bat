c03bf6cc863115df5caac2df4cead647abb4cce0
Manuel Grosso <manuel.grosso@CPT.local>
2021-10-14 16:27:38 +0200

    commit 14.10.21

--- copy.bat
+++ copy.bat
@@ -1,5 +1,5 @@
 :loop
 
-xcopy /s/y "D:\Scuola\I3\Mod 306-Progetti\ChromeDinoHotseat\src\" "C:\xampp\htdocs\"
+xcopy /s/y "D:\Scuola\I3\Mod 306-Progetti\ChromeDinoHotseat\src\ChromeDino.html" "C:\xampp\htdocs\"
 
 goto loop
